a9bc5ba269a6f25f4a4b1b6f172b516d96d42556
Angela Julie Dobbs <ajdobbs.sm@gmail.com>
2016-11-26 22:14:02 -0500

    Have levels and categories correctly parsing

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -13,11 +13,26 @@ angular.module('myApp.view1', ['ngRoute'])
   function(commService, parseService, $scope, $sce) {
 
     var options = ['company', 'category', 'level', 'location']
-    $scope.options = {};
+
+    $scope.categories = ['Account Management', 'Business & Strategy', 'Creative & Design',
+    'Customer Service', 'Data Science', 'Editorial', 'Education', 'Engineering',
+    'Finance', 'Fundraising & Development', 'Healthcare & Medicine', 'HR & Recruiting',
+    'Legal', 'Marketing & PR', 'Operations', 'Project & Product Management',
+    'Retail', 'Sales', 'Social Media & Community']
+
+
+    $scope.options = {
+      level: [],
+      category: [],
+      company: [],
+      location: []
+    };
     $scope.page = 1;
 
-    function fireSearch() {
+    $scope.fireSearch = function () {
+      console.log(buildApiEndpoint());
       commService.get(buildApiEndpoint()).then(function(response) {
+        console.log(response);
         $scope.results = response.data.results;
         $scope.results.forEach(function(result) {
           result.trustedResult = $sce.trustAsHtml(result.contents)
@@ -29,14 +44,29 @@ angular.module('myApp.view1', ['ngRoute'])
       var base = 'https://api-v2.themuse.com/jobs?'
       options.forEach(function(option) {
         if ($scope.options[option] && $scope.options[option].length) {
-          $scope.options[option].params.forEach(function(param) {
-            base += option + "=" + param + "&";
+          $scope.options[option].forEach(function(param) {
+            // if (param.indexOf('&') > -1) {
+            //   param.splice(param.indexOf('&'))
+            // }
+            base += option + "=" + encodeURIComponent(param) + "&";
           })
         }
       })
       return base + "page=" + $scope.page;
     }
 
-    fireSearch()
+    $scope.handleCheckbox = function(option, value) {
+      console.log(option, value);
+      console.log($scope.options[option]);
+      var idx = $scope.options[option].indexOf(value);
+       if (idx > -1) {
+         $scope.options[option].splice(idx, 1);
+       } else {
+         $scope.options[option].push(value);
+       }
+      console.log($scope.options);
+    }
+
+    // fireSearch()
 
 }]);
