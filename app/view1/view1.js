181a4d02f160956bd46973c85996141bd66532b2
Angela Julie Dobbs <ajdobbs.sm@gmail.com>
2016-11-27 21:45:23 -0500

    Added next and previous page as well as company search

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -16,13 +16,15 @@ angular.module('myApp.view1', ['ngRoute'])
 
     $scope.levels = ['Internship', 'Entry Level', 'Mid Level', 'Senior Level']
 
+
+    //As there didn't appear to be any way to query for the allowed parameters to just set it up, we wrote it out this way so it only needed to be written out once
     $scope.categories = ['Account Management', 'Business & Strategy', 'Creative & Design',
     'Customer Service', 'Data Science', 'Editorial', 'Education', 'Engineering',
     'Finance', 'Fundraising & Development', 'Healthcare & Medicine', 'HR & Recruiting',
     'Legal', 'Marketing & PR', 'Operations', 'Project & Product Management',
     'Retail', 'Sales', 'Social Media & Community']
 
-
+    //The location choices were limited to those on the dropdown on The Muse
     $scope.locations = ['New York City Metro Area', 'San Francisco Bay Area',
     'Los Angeles, CA', 'Chicago, IL', 'Flexible / Remote', 'Washington DC Metro Area',
     'Austin, TX', 'Boston Metro Area', 'Atlanta, GA', 'Seattle, WA', 'Bellevue, WA']
@@ -36,9 +38,7 @@ angular.module('myApp.view1', ['ngRoute'])
     $scope.page = 1;
 
     $scope.fireSearch = function () {
-      console.log(buildApiEndpoint());
       commService.get(buildApiEndpoint()).then(function(response) {
-        console.log(response);
         $scope.results = response.data.results;
         $scope.results.forEach(function(result) {
           result.trustedResult = $sce.trustAsHtml(result.contents)
@@ -59,17 +59,29 @@ angular.module('myApp.view1', ['ngRoute'])
     }
 
     $scope.handleCheckbox = function(option, value) {
-      console.log(option, value);
-      console.log($scope.options[option]);
       var idx = $scope.options[option].indexOf(value);
        if (idx > -1) {
          $scope.options[option].splice(idx, 1);
        } else {
          $scope.options[option].push(value);
        }
-      console.log($scope.options);
     }
 
-    // fireSearch()
+    $scope.prevPage = function() {
+      if ($scope.page > 1) {
+        $scope.page -= 1;
+        $scope.fireSearch();
+      }
+    }
+
+    $scope.nextPage = function() {
+      //It is possible if the parameters have changed that going to the next page
+      //with a different search may produce strange results, and with more time
+      //this could be architected to compare previous endpoints and check if they
+      //have changed but that didn't seem neccessary for v1
+      $scope.page += 1;
+      $scope.fireSearch();
+    }
+
 
 }]);
