16d2ebdf95d83516425bd865e166c2af72e521b9
Angela Dobbs <ajdobbs.sm@gmail.com>
2017-01-10 13:31:29 -0500

    added basic functionality for manually adding listings to the db

--- app/listings/listings.js
+++ app/listings/listings.js
@@ -9,12 +9,25 @@ angular.module('myApp.list', ['ngRoute'])
   });
 }])
 
-.controller('ListCtrl', ['commService', 'parseService', '$scope', '$sce',
-  function(commService, parseService, $scope, $sce) {
+.controller('ListCtrl', ['commService', 'parseService', '$scope', '$sce', '$route',
+  function(commService, parseService, $scope, $sce, $route) {
 
     commService.get('http://localhost:5000/listings').then(function(data) {
       console.log(data);
       $scope.listings = data.data.listings;
     })
+
+    $scope.addListing = function() {
+      var payload = {
+        ListingName: $scope.ListingName,
+        ListingCompany: $scope.ListingCompany,
+        Link: $scope.Link,
+        ResultPage: $scope.ResultPage,
+      }
+      console.log(payload);
+      commService.post('http://localhost:5000/listings', payload).then(function(response) {
+        $route.reload();
+      })
+    }
 
 }]);
