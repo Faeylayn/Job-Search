4acf18785a4d0f752dd87abbf6c856d920110a37
Angela Dobbs <ajdobbs.sm@gmail.com>
2017-01-19 17:15:16 -0500

    added a comment for TODO

--- app/detail/detail.js
+++ app/detail/detail.js
@@ -12,8 +12,8 @@ angular.module('myApp.detail', ['ngRoute'])
 .controller('DetailCtrl', ['commService', '$scope', '$sce', '$location',
   function(commService, $scope, $sce, $location) {
 
-
-      commService.get('http://localhost:5000/').then(function(response) {
+      //need to grab url param for id to get the correct listing from the db
+      commService.get('http://localhost:5000/detail').then(function(response) {
         console.log(response);
         $scope.result = response.data;
           $scope.result.trustedResult = $sce.trustAsHtml($scope.result.ResultPage)
